897f06282a3e8d72daf5b08f227669d186c096d8
Anh Mai <theanht1k20@gmail.com>
2017-04-11 08:26:32 +0700

    feat(routes): Chane root

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # resources :dashboards
 
+  root to:'dashboards#index'
+
   get '/dashboards', to: 'dashboards#index'
   post '/dashboards/create', to: 'dashboards#create'
   post '/dashboards/upload', to: 'dashboards#upload'
